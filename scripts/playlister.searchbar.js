99d9e12e96cd181555b4314abb6872d747277b7e
joheenc <joheen.c@gmail.com>
2015-10-04 05:43:05 -0400

    I helped dad

--- scripts/playlister.searchbar.js
+++ scripts/playlister.searchbar.js
@@ -11,18 +11,17 @@ $(document).ready(function(){
           var holder = $("#main_container_container").position().top;
           g = 1;
         }
-$("#Main_Searchbar").css("top",searchtop).css("left",$("#Main_Searchbar").position().left).css("position","absolute").fadeOut(500);
+        $("#Main_Searchbar").css("top",searchtop).css("left",$("#Main_Searchbar").position().left).css("position","absolute").fadeOut(500);
         $("#main_container_container").css("position","absolute").css("top",holder).animate({
           top:0
         },1000,function(){
-          console.log("hfjhiwafkha");
           loader();
         })
 
 
         /*
           */
-
+          loaddata();
     }
   });
     $("#Main_Searchbar").click(function(){
@@ -34,34 +33,38 @@ function loader(){
   var x = $("html").height();
   console.log();
   if(t==0){
-    $("#main_container").append($("<img style='display:none' id='loading' src='images/load.gif'/>").fadeIn(1000));
+    $("#main_container").append($("<img style='display:none' id='loading' src='images/load2.gif'/>").fadeIn(1000));
     $("#loading").css("left",$("html").width()/2-$("#loading").width()/2);
     $("#loading").css("top",$("html").height()/2-$("#loading").height()/2)
     t = 1;
   }
 }
 function loaddata(){
-  $.get("/fetch", {phonenumber:$("#Main_Searchbar").val()},function(data){
-    finishedload(data);
-  });
+    var request = $.get("/fetch", {Phonenum:$("#Main_Searchbar").val()});
+        open_white_section()
+        finishedload("");
+      }
+
+function open_white_section(){
+  $("#loading").fadeOut("slow",function(){
+      $("#loading").remove();
+      var width = $(".title_sides").width()*2+$("#main_title").width()+20;
+      var height = $("html").height()-$("#main_container_container").height();
+      var htmlheight = $("html").height();
+      var ok = $("#main_container_container").height();
+      console.log(htmlheight)
+      $("#main_container").append("<div id='returned_data' style='border-radius:1em;position:relative;top:"+htmlheight+";background-color:#DDD1E1;width:"+width+";height:"+height+"'></div>")
+      $("#returned_data").animate({
+        top:$("#main_container_container").height()
+      },500);
+    });
 }
 function finishedload(){
-   $("#loading").fadeOut("slow",function(){
-       $("#loading").remove();
-       var width = $(".title_sides").width()*2+$("#main_title").width()+20;
-       var height = $("html").height()-$("#main_container_container").height();
-       var htmlheight = $("html").height();
-       var ok = $("#main_container_container").height();
-       console.log(htmlheight)
-       $("#main_container").append("<div id='returned_data' style='border-radius:1em;position:relative;top:"+htmlheight+";background-color:#DDD1E1;width:"+width+";height:"+height+"'></div>")
-       $("#returned_data").animate({
-         top:$("#main_container_container").height()
-       },500);
+
        var data_extracted = JSON.parse(data);
        for(var d = 0;d<data_extracted.length();d++){
           add_data("https://www.youtube.com/watch?v="+data_extracted[d],"http://img.youtube.com/vi/"+data_extracted[d]+"/0.jpg","");
        }
-   });
 }
 
 function add_data(url, imageurl, desc){
